f171b5ceabe522ded3238200b6d86b8b1a1d1133
naoki-shigehisa <naoki.shigehisa@gmail.com>
2023-05-05 20:16:47 +0900

    fix sitemap

--- front/src/next-sitemap.config.js
+++ front/src/next-sitemap.config.js
@@ -1,15 +1,16 @@
 module.exports = {
   siteUrl: 'https://www.cockhome.com',
-  additionalPaths: siteUrls,
+  additionalPaths: async () => {
+    const fields = [];
+    const recipesCount = 4;
+    for (var i = 1; i <= recipesCount; i++) {
+      fields.push({
+        loc: '/recipes/' + i,
+        priority: 0.8,
+      })
+    }
+  
+    return fields;
+  },
   exclude: [],
-};
-
-const siteUrls = async () => {
-  const fields = [];
-  const recipesCount = 4;
-  for (var i = 1; i <= recipesCount; i++) {
-    fields.push('/recipes/' + i);
-  }
-
-  return fields;
 };
