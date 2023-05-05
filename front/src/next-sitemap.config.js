33577d4144b040c432b8df50b2b99b0abca0167d
naoki-shigehisa <naoki.shigehisa@gmail.com>
2023-05-05 20:05:14 +0900

    fix sitemap

--- front/src/next-sitemap.config.js
+++ front/src/next-sitemap.config.js
@@ -1,6 +1,17 @@
+import { getRecipesCount } from "@/lib/cocktailServerClient"
+
 module.exports = {
   siteUrl: 'https://www.cockhome.com',
-  outDir: './public/sitemap',
-  sitemapBaseFileName: 'static',
+  additionalPaths: siteUrls,
   exclude: [],
+};
+
+const siteUrls = async () => {
+  const fields = [];
+  const recipesCount = await getRecipesCount();
+  for (var i = 1; i <= recipesCount; i++) {
+    fields.push('/recipes/' + i);
+  }
+
+  return fields;
 };
